594ca22bd1562d7fa5d01b47caab27a39c6afec9
PRINCE NAYAK <113225737+nayakp-854@users.noreply.github.com>
2024-03-06 10:05:17 +0530

    F

--- server.js
+++ server.js
@@ -21,6 +21,15 @@ const URL = process.env.MONGODB_URL;
 //   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 // });
 
+if (process.env.NODE_ENV === "production") {
+  //*Set static folder up in production
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
+
 mongoose
   .connect(URL)
   .then(() => {
